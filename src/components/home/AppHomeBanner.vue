be4d2ced1da83320beb12c9575d7d534c48b744e
Roman Menshikov <R-Tupac@yandex.ru>
2023-10-01 17:35:17 +0300

    final work fix-5

--- src/components/home/AppHomeBanner.vue
+++ src/components/home/AppHomeBanner.vue
@@ -7,24 +7,26 @@
           There are many variations of the passages of lorem Ipsum
           fromavailable, majority.
         </div>
-        <button class="banner__btn btn-reset">
-          <span class="banner__btn-text">Get Started</span>
-          <svg
-            width="19"
-            height="17"
-            viewBox="0 0 19 17"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M2 8.43542L15.7232 8.29857M10.6182 1.91138L17.1412 8.43436L10.4868 15.0887"
-              stroke="#CDA274"
-              stroke-width="2"
-              stroke-linecap="square"
-              stroke-linejoin="round"
-            />
-          </svg>
-        </button>
+        <router-link to="/project">
+          <button class="banner__btn btn-reset">
+            <span class="banner__btn-text">Get Started</span>
+            <svg
+              width="19"
+              height="17"
+              viewBox="0 0 19 17"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M2 8.43542L15.7232 8.29857M10.6182 1.91138L17.1412 8.43436L10.4868 15.0887"
+                stroke="#CDA274"
+                stroke-width="2"
+                stroke-linecap="square"
+                stroke-linejoin="round"
+              />
+            </svg>
+          </button>
+        </router-link>
       </div>
     </div>
   </div>
