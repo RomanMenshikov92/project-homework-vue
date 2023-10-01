4c6164f07f50084530435b66bbdc9f80223e0cbe
Roman Menshikov <R-Tupac@yandex.ru>
2023-10-01 05:10:37 +0300

    add gh-pages fix-1

--- vue.config.js
+++ vue.config.js
@@ -3,20 +3,3 @@ module.exports = defineConfig({
   publicPath: '/project-homework-vue/',
   transpileDependencies: true,
 });
-// module.exports = {
-//   css: {
-//     loaderOptions: {
-//       // передача настроек в sass-loader
-//       // @/ это псевдоним к каталогу src/ поэтому предполагается,
-//       // что у вас в проекте есть файл `src/variables.scss`
-//       // Примечание: эта опция называется "prependData" в sass-loader v8
-//       sass: {
-//         additionalData: `@import "~@/src/assets/sass/_vars.sass";`,
-//         additionalData1: `@import "~@/src/assets/sass/_setting.sass";`,
-//         additionalData2: `@import "~@/src/assets/sass/_fonts.sass";`,
-//         additionalData3: `@import "~@/src/assets/sass/_mixins.sass";`,
-//       },
-//       // передача настроек Less.js в less-loader
-//     },
-//   },
-// };
