e91f9a4d86f045260d6e1492bee7f7673ccf91b7
Roman Menshikov <R-Tupac@yandex.ru>
2023-10-01 13:48:52 +0300

    added fix_3

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,7 @@
 const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
   // publicPath: '/project-homework-vue/',
-  publicPath: process.env.NODE_ENV === 'production'
-  ? '/project-homework-vue/'
-  : '/',
+  publicPath:
+    process.env.NODE_ENV === 'production' ? '/project-homework-vue/' : '/',
   transpileDependencies: true,
 });
