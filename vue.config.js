5c354d5e346d0ff2109455df2ade15b9b6735d16
Roman Menshikov <R-Tupac@yandex.ru>
2023-10-01 04:48:24 +0300

    add gh-pages

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,7 @@
-const { defineConfig } = require("@vue/cli-service");
+const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
+  publicPath: '/project-homework-vue/',
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === 'production'
-  ? '/project-homework-vue/'
-  : '/'
 });
 // module.exports = {
 //   css: {
